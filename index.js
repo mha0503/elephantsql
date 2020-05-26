18bbd7783dd41154a3380d84ccf8af6abac34f41
Martina Hahler <martinahahler@Martinas-MacBook-Pro.local>
2020-05-26 15:18:50 +0200

    5 commit

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const { Pool } = require('pg')
 
 const pool = new Pool()
 
+var bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res)=> {
     pool
@@ -16,11 +22,27 @@ app.get('/', (req, res)=> {
 app.get('/', (req, res)=> {
     const {id} = req.params
     pool
-        .query('SELECT * FROM user WHERE id=$1', [id])
+        .query('SELECT * FROM users WHERE id=$1', [id])
         .then(data => res.json(data.rows))
         .catch(err => console.log(err))
 })
 
+app.post('/', (req, res)=> {
+    const {first_name, last_name, age} = req.body
+    pool
+        .query('INSERT INTO users (first_name, last_name, age) Values($1,$2,$3) RETURNING *', [first_name, last_name, age])
+        .then(data => res.json(data.rows))
+        .catch(err => console.log(err))
+})
+
+app.put('/:id', (req, res)=> {
+    const {id} = teq.params
+    const {first_name, last_name, age} = req.body
+    pool
+        .query('UPDATE users SET first_name=$1, last_name=$2, age=$3 WHERE id=$4 Values($1,$2,$3) RETURNING *', [first_name, last_name, age, id])
+        .then(data => res.json(data.rows))
+        .catch(err => console.log(err))
+})
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=>{
